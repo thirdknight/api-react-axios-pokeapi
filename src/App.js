54400f0499e5c04262db640193ed457166dbf4eb
Edinson <edinson.ningles@gmail.com>
2024-03-27 11:00:14 +0100

    Added end tuto

--- src/App.js
+++ src/App.js
@@ -3,12 +3,61 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
+  
+
+  const searchPokemon = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    // .then(response => console.log(response))
+    .then(response => {
+      setPokemon({
+        name: pokemonName,
+        species: response.data.species.name,
+        img: response.data.sprites.front_default,
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        type: response.data.types[0].type.name,
+      });
+      setPokemonChosen(true);
+    })
+  }
+
   return(
     <div className="App">
       <div className="TitleSection">
         <h1>Pokemon Stats</h1>
-        <input type={"text"} />
-        <button>Search Pokemon</button>
+        <input type={"text"}
+               onChange={(event) => {
+                setPokemonName(event.target.value)
+                }}
+        />
+        <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+      <div className='DisplaySection'>
+        {!pokemonChosen ? (
+          <h1>Please choose a Pokemnon</h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img} alt=''/>
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type:{pokemon.type}</h3>
+            <h4>Hp: {pokemon.hp}</h4>
+            <h4>Attack: {pokemon.attack}</h4>
+            <h4>Defense: {pokemon.defense}</h4>
+          </>
+        )}
       </div>
   </div>
   )
